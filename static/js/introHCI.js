4f3858658cb4976c7007e971ceb69f1b09c3411e
davidwu@ucsd.edu <davidwu@ucsd.edu>
2018-01-24 20:25:52 -0800

    lab 3 submission

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,30 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+function projectClick(e) { 
+	// prevent the page from reloading 
+	e.preventDefault();
+	// In an event handler, $(this) refers to 
+	// the object that triggered the event 
+	var containingProject = $(this).closest(".project");
+	 var description = $(containingProject).find(".project-description");
+	 if (description.length == 0) {
+			$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	 } else {
+		 $(".project-description").fadeOut();
+	 }
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$("a.thumbnail").click(projectClick);
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").text("hello");
 	});
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
